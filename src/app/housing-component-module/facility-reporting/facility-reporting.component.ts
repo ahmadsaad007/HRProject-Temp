d0f6cb050b4cf1b6b1a55b5f47444af42850ee17
ahmadsaad007 <ahmadsd97@gmail.com>
2020-09-25 10:28:55 -0400

    Housing and Facility Page partially completed. Facing issues with Displaying FormArray

--- src/app/housing-component-module/facility-reporting/facility-reporting.component.ts
+++ src/app/housing-component-module/facility-reporting/facility-reporting.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-facility-reporting',
@@ -7,9 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FacilityReportingComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  formList: FormArray;
+  value;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.value = this.getJson();
+    this.form= this.fb.group({
+      title: this.fb.control(''),
+      description: this.fb.control('')
+    });
+
+    this.formList=this.fb.array([]);
+    //this.form.setControl('addresses', this.fb.array(this.value || []));
+    this.addItem();
+    //console.log(this.formList.controls);
+  }
+
+  addItem(){
+    
+    for(var i =0; i< this.value.length;i++){  
+      var fg=this.fb.group({
+        fl_title: this.value[i].title,
+        fl_description: this.value[i].title,
+        fl_date: this.value[i].title,
+        fl_created_by: this.value[i].title,
+        fl_status: this.value[i].title 
+      });
+      //fg.disable();
+      this.formList.push(fg);
+      //this.formList.setValue([this.value[i].title,this.value[i].description,this.value[i].created_by, this.value[i].date, this.value[i].status]);
+    }
+    return;
+  }
+  getJson(){
+    return [{title:'Plumbing', description: 'broken pipe','created_by':'user1','date':'12/12/2004','status':'close'},
+    {title:'Toilet clog', description: 'master room toilet clog','created_by':'user1','date':'12/12/2002','status':'open'}];
+  }
+
+  onSubmit(mediaItem){
+    console.log(mediaItem)
   }
 
 }
--- src/app/housing-component-module/housing-details/housing-details.component.ts
+++ src/app/housing-component-module/housing-details/housing-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-housing-details',
@@ -9,17 +9,42 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class HousingDetailsComponent implements OnInit {
 
   form: FormGroup;
+  employees: FormArray
   constructor(private formBuilder: FormBuilder) { }
+
+  housing= {'address':
+            {'addressLine1': '58',
+             'addressLine2': 'Shelley Circle',
+              'city':'East Windsor',
+              'state': 'NJ',
+              'zip': '08520'
+            },
+            'employees':['name1', 'name2', 'name3']
+          }
+
+  
 
   ngOnInit(){
     this.form = this.formBuilder.group({
-      houseNumber: this.formBuilder.control('58'),
-      houseStreet: this.formBuilder.control('Shelley Circle'),
-      houseCity: this.formBuilder.control('East Windsor'),
-      houseState: this.formBuilder.control('NJ'),
-      houseZip: this.formBuilder.control('08520')
+      houseNumber: this.formBuilder.control(this.housing.address.addressLine1),
+      houseStreet: this.formBuilder.control(this.housing.address.addressLine2),
+      houseCity: this.formBuilder.control(this.housing.address.city),
+      houseState: this.formBuilder.control(this.housing.address.state),
+      houseZip: this.formBuilder.control(this.housing.address.zip),
     });
-    this.form.get('houseNumber').disable;
+    this.form.disable();
+    
+    //this.employees= this.formBuilder.array([this.addEmployees()]);
   }
+
+  // addEmployees() {
+  //   for(var i=0; i< this.housing.employees.length;i++){
+  //     this.employees.push(new FormControl(this.housing.employees[i]));
+  //   }
+  // }
+
+  // get employees(){
+  //   return this.employees;
+  // }
 
 }
